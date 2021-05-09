f4d2e8ee17aa9cf884c7ec4658ab561c41c09e82
Martin Faroppa <mafaroppa@gmail.com>
2021-05-08 21:05:57 -0300

    functionality in market component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
-import { useEffect, useState } from 'react';
-import { LineChart, Line, YAxis } from 'recharts';
 import './App.css';
-
-const PRICE_CALCULATION_TIME = 500;
-const PRICES_LENGTH = 365;
-const TIME_PERIOD = 1/365;
-const STANDARD_DEVIATION = Math.sqrt(TIME_PERIOD)
+import Market from './Market';
 
 function App(props) {
 
-	let [time, setTime] = useState(0)
-	let [currentPrice, setCurrentPrice] = useState(500)
-	let [prices, setPrices] = useState([{
-		time: 0,
-		price: 500
-	}])
-
-	function generateGaussian(mean, std) {
-		var u1 = Math.random();
-		var u2 = Math.random();
-		var z = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(Math.PI * 2 * u2);
-		return z * std + mean;
-	}
-
-	let generatePrice = () => {
-		const lastPrice = prices[prices.length - 1].price
-		const variation = lastPrice * TIME_PERIOD * props.drift + lastPrice * props.volatility * generateGaussian(0, STANDARD_DEVIATION);
-		setPrices(oldPrices => [...oldPrices.slice(Math.max(oldPrices.length - PRICES_LENGTH-1, 0)), {
-			time: time + TIME_PERIOD,
-			price: lastPrice + variation
-		}])
-	}
-
-	useEffect(() => {
-		setCurrentPrice(prices[prices.length - 1].price)
-	}, [prices])
-
-	//timer for new price calculation
-	useEffect(() => {
-		let interval = setInterval(() => {
-			setTime(time + TIME_PERIOD)
-			generatePrice()
-		}, PRICE_CALCULATION_TIME)
-
-		return () => {
-			clearInterval(interval);
-		}
-	}, [prices])
-
 	return (
 		<div className="App">
-			{currentPrice}
-			<LineChart width={1000} height={300} data={prices}>
-				<YAxis/>
-				<Line type="monotone" dataKey="price" isAnimationActive={false} dot={false} stroke="#8884d8" />
-			</LineChart>
+			<Market drift={0.15} volatility={0.1}/>
+			<Market drift={0.35} volatility={0.22}/>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App drift={0.24} volatility={0.08}/>
-    <App drift={0.8} volatility={0.25}/>
-    <App drift={0.5} volatility={0.6}/>
-    <App drift={0.1} volatility={0.2}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
